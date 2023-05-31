a8806c0ec075c7e77912779a5b8a87c41ff31da0
Emeraude Makola <76725554+Emeraude1@users.noreply.github.com>
2023-05-31 20:51:03 +0100

    Add files via upload

---
+++ UI design/View/UCs/UC_Login.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI_design.View.UCs
+{
+    /// <summary>
+    /// Interaction logic for UC_Login.xaml
+    /// </summary>
+    public partial class UC_Login : UserControl
+    {
+        public UC_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtUser.TextChanged += Conttols_TextChanged;
+
+
+        }
+
+
+        #region btnLogin Enabled
+        private void Conttols_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateButtonSate();
+        }
+        private void UpdateButtonSate()
+        {
+            try
+            {
+                string username = txtUser.Text;
+                //string password = txtPassword.Text;
+
+                if (!string.IsNullOrEmpty(username) /*&&
+                    !string.IsNullOrEmpty(password)*/)
+
+                {
+                    btnLogin.IsEnabled = true;
+                }
+                else
+                {
+                    btnLogin.IsEnabled = false;
+                }
+            }
+            catch
+            {
+
+            }
+
+        }
+
+
+        #endregion
+    }
+}
